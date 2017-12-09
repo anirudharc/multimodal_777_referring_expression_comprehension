c5ff6cc08ac84b1444214efcf95d5c2da9991e74
Anirudha Rayasam <rcanirudha@RCs-MacBook-Pro.wv.cc.cmu.edu>
2017-12-08 21:48:32 -0500

    updated the script for proposal generation and changed proposals

--- google_refexp/preprocess.py
+++ google_refexp/preprocess.py
@@ -5,11 +5,11 @@ import json
 train_data = json.load(open('google_refexp_train_201511_coco_aligned_multibox_umd.json'))
 val_data = json.load(open('google_refexp_val_201511_coco_aligned_multibox_umd.json'))
 
-train_images = ['dataset/' + image_id for image_id in train_data['images']]
-val_images = ['dataset/' + image_id for image_id in val_data['images']]
+train_images = ['dataset/train2014/' + image_id for image_id in train_data['images']]
+val_images = ['dataset/val2014/' + image_id for image_id in val_data['images']]
 
-train_images_filename = ['dataset/' + train_data['images'][image_id]['file_name'] for image_id in train_data['images']]
-val_images_filename = ['dataset/' + val_data['images'][image_id]['file_name'] for image_id in val_data['images']]
+train_images_filename = ['dataset/train2014/' + train_data['images'][image_id]['file_name'] for image_id in train_data['images']]
+val_images_filename = ['dataset/val2014/' + val_data['images'][image_id]['file_name'] for image_id in val_data['images']]
 
 train_bboxes_list = []
 for image_id in train_data['images']:
